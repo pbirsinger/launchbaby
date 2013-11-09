f10676f74f289ef275729fcf85e63e8930037088
Peter Birsinger <pbirsinger@berkeley.edu>
2013-11-09 20:42:41 +0000

    add webpage schema

---
+++ models/webpage.js
@@ -0,0 +1,18 @@
+//Module dependencies
+
+var mongoose = require('mongoose')
+    , Schema = mongoose.Schema
+
+//Getters
+//Setters
+
+//Schema
+var WebpageSchema = new Schema({
+   url:         {type : String, trim : true},
+  createdAt:    {type : Date, default: Date.now}
+
+//Validation
+//Methods
+//Statics
+
+mongoose.model('Webpage', WebpageSchema)

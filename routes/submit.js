9dc16ceb69229d2f9dbb5facf9f7f9094aabca65
Alex Hu <a@alexhu.me>
2013-11-09 12:46:35 -0800

    Monkey p

--- routes/submit.js
+++ routes/submit.js
@@ -1,8 +1,8 @@
 var mongoose    = require('mongoose'),
-    Comment     = mongoose.model('Comment')
+    //Comment     = mongoose.model('Comment')
 
 exports.post = function(req, res){
-  var comment = new Comment(req.body)
+  //var comment = new Comment(req.body)
   //res.send("posting");
   res.send({status: 'success'})
 };

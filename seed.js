5b93a8d2c1497327e13c3e082ec88322cb66f2a8
Peter Birsinger <pbirsinger@berkeley.edu>
2013-11-10 01:19:05 +0000

    add seed.js file

---
+++ seed.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var users = require('../models/user.js');
+var User = mongoose.model('User');
+
+var u = new User(
+	name: "pistola",
+	email: "hi@gmail.com"
+);
+
+u.save(function (err, c) {
+  if (err) {res.send({status: 'Failure saving user: ' + err});}
+  console.log("saved user");
+});

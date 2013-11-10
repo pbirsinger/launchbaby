01e0e2a93c2dc9a9cd162377aa2eae8cf01a957f
Peter Birsinger <pbirsinger@berkeley.edu>
2013-11-10 01:09:04 +0000

    posting works.
    example usage:
    curl -d '{"body": ["from rob"], "email":["hi@gmail.com"]}' -H
    'content-type:application/json' http://localhost:1337/url/32

--- routes/submit.js
+++ routes/submit.js
@@ -1,16 +1,26 @@
-var webpage = require('../models/webpage.js');
+var mongoose = require('mongoose');
 var comments = require('../models/comment.js');
 var users = require('../models/user.js');
-var mongoose = require('mongoose');
+var Comment = mongoose.model('Comment');
+var User = mongoose.model('User');
 
 exports.post = function(req, res){
-  var urll = /^\/url\/(.*)/.exec(req.url)[1];
-  var Comment = mongoose.model('Comment');
+  var url = /^\/url\/(.*)/.exec(req.url)[1];
   var comment = new Comment();
-  comment.body = "this is a comment";
-  comment.user = "user1";
-  comment.page = "www.google.com";
-  comment.save(function () {
-    res.send({status: 'success'});
+  comment.body = req.body['body'];
+  User.find({email: req.body['email']}, function(err, user){
+    if (err) {
+      res.send({status: 'Error looking up user: ' + err});
+      return;
+    } else if (user.length == 0) {
+      res.send({status: 'No user found with email ' + req.body['email']});
+      return;
+    }
+    comment.user = user[0]._id;
+    comment.page = url;
+    comment.save(function (err, c) {
+      if (err) {res.send({status: 'Failure saving comment: ' + err});}
+      res.send({status: 'success: ' + c});
+    });
   });
 };

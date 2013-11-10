7390196582073d72669553843db32c44c77598a2
Peter Birsinger <pbirsinger@berkeley.edu>
2013-11-10 04:08:10 +0000

    add netVotes and replyTo fields to comment. small other changes

--- models/comment.js
+++ models/comment.js
@@ -8,10 +8,12 @@ var mongoose = require('mongoose')
 
 //Schema
 var CommentSchema = new Schema({
-  body:         {type : String, default : '', trim : true},
   user:         {type : Schema.ObjectId, ref: 'User'},
+  url:          {type : String, trim : true},
   selector:     {type : String, default : '', trim : true},
-  page:         {type : String, trim : true},
+  body:         {type : String, default : '', trim : true},
+  netVotes:     {type: Number, default: 0},
+  replyTo:      {type : Schema.ObjectId, ref: 'Comment'},
   createdAt:    {type : Date, default: Date.now}
 });
 
--- routes/submit.js
+++ routes/submit.js
@@ -5,9 +5,6 @@ var Comment = mongoose.model('Comment');
 var User = mongoose.model('User');
 
 exports.post = function(req, res){
-  var url = /^\/url\/(.*)/.exec(req.url)[1];
-  var comment = new Comment();
-  comment.body = req.body['body'];
   User.find({email: req.body['email']}, function(err, user){
     if (err) {
       res.send({status: 'Error looking up user: ' + err});
@@ -16,11 +13,15 @@ exports.post = function(req, res){
       res.send({status: 'No user found with email ' + req.body['email']});
       return;
     }
-    comment.user = user[0]._id;
-    comment.page = url;
+    var comment = new Comment({
+      body: req.body['body'],
+      user: user[0]._id,
+      url: /^\/url\/(.*)/.exec(req.url)[1],
+      replyTo: req.body['replyTo']
+    });
     comment.save(function (err, c) {
-      if (err) {res.send({status: 'Failure saving comment: ' + err});}
-      res.send({status: 'success: ' + c});
+      if (err) res.send({status: 'Failure saving comment: ' + err});
+      else res.send({status: 'success: ' + c});
     });
   });
-};
+}

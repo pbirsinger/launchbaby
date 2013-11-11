10d843be7dc3bdb27ec5114f779bd9164d265ee6
Peter Birsinger <pbirsinger@berkeley.edu>
2013-11-11 00:45:27 +0000

    updated basic tests

--- basicTests.sh
+++ basicTests.sh
@@ -2,7 +2,7 @@ echo "\nFetching comments..................................."
 curl localhost:1337/url/http://www.nytimes.com/2013/11/10/world/iran-nuclear-talks.html
 
 echo "\nCreating a comment...................................."
-curl -d '{"body": ["from rob"], "selector": ["hiiiii"],"email":["pen@gmail.com"], "replyTo":["527ed7388f5e7b28d5000001"]}' -H 'content-type:application/json' http://localhost:1337/url/32
+curl -d '{"body": ["from rob"], "paraNum": [2],"email":["pen@gmail.com"], "replyTo":["527ed7388f5e7b28d5000001"]}' -H 'content-type:application/json' http://localhost:1337/url/32
 
 echo "\nCreating a user....................................."
 curl -d '{"name": ["rob"], "email": ["balickileaky"]}' -H 'content-type:application/json' http://localhost:1337/user
@@ -11,4 +11,4 @@ echo "\nFetching webpages list......................................"
 curl localhost:1337/webpage
 
 echo "\nVoting on a comment.........................................."
-curl -d '{"direction": ["up"], "id":["527ffabf153f730000000009"]}' -H 'content-type:application/json' http://localhost:1337/vote
+curl -d '{"direction": ["up"], "id":["52801edc7d1a590000000009"]}' -H 'content-type:application/json' http://localhost:1337/vote
